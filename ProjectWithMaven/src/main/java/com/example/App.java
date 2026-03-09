0b87756d19fd167045e7acffeac94bed984024fb
TTN <terra_p@mail.ru>
2026-03-09 22:13:41 +0300

    JDBC_1: Добавить 5 произвольных Person использовать JDBC

--- ProjectWithMaven/src/main/java/com/example/App.java
+++ ProjectWithMaven/src/main/java/com/example/App.java
@@ -21,16 +21,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static java.time.LocalTime.now;
+
 public class App {
     public static void main(String[] args) throws SQLException {
         ResourceBundle resource = ResourceBundle.getBundle("DB_people");
         String url = resource.getString("url");
-        String username = resource.getString("user");
+        String userName = resource.getString("user");//чуть ниже ввеле
         String pass = resource.getString("password");
-        try (Connection conn = DriverManager.getConnection(url,
-                username, pass)) {
-        //    printMetaData(conn);
+        try (Connection conn = DriverManager.getConnection(url, userName, pass);
+             Statement st = conn.createStatement()) {
+            //    printMetaData(conn);
+            try (ResultSet rs = st.executeQuery("SELECT * FROM person")) {//работа с данными
+                ResultSetMetaData rsmd = rs.getMetaData();//работа со структурой
+                int number = rsmd.getColumnCount();
+                while (rs.next()) {
+                    for (int i = 1; i <= number; i++) {
+                        String columnName = rsmd.getColumnName(i);
+                        System.out.print(columnName + " " + rs.getString(i) + " ");
+                        // System.out.print(" " + rs.getString(i));//доставать через индекс
+                        //  System.out.println(" " + rs.getString(columnName));//доставать через имя колонки
+                    }
+                    System.out.println();
+                }
+            }
+            //addFivePersons(st);
+
         }
+    }
+
+    private static void addFivePersons(Statement st) throws SQLException {
+        st.executeUpdate("INSERT INTO person (age, salary, passport, addres, dateOfBirthday, dateTimeCreate, timeToLunch, letter) VALUES" +
+         "(22, 11000, 'MP135236', 'st_Pobedy5', '2005-01-02', now(), '131500', 'шестой пошел')," +
+         "(32, 21000, 'MP235236', 'st_Pobedy6', '1995-01-02', now(), '141500', 'седьмой пошел')," +
+         "(42, 31000, 'MP335236', 'st_Pobedy7', '1985-01-02', now(), '151500', 'восьмой пошел')," +
+         "(52, 41000, 'MP435236', 'st_Pobedy8', '1975-01-02', now(), '161500', 'девятый пошел')," +
+         "(62, 51000, 'MP535236', 'st_Pobedy9', '1965-01-02', now(), '171500', 'десятый пошел')");
     }
 
     private static void printMetaData(Connection conn) throws SQLException {
