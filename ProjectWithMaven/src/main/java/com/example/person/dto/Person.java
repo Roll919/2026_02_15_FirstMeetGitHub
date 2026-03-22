4c66e110bf85fad96dda2b9e29a46a39b72ccd13
TTN <terra_p@mail.ru>
2026-03-22 19:43:58 +0300

    Branch_App5DAO 1 Подтянут Lombok в pom.xml 2 Добавлены аннотации Lombok в класс Person(dto) с удалением сорного кода

--- ProjectWithMaven/src/main/java/com/example/person/dto/Person.java
+++ ProjectWithMaven/src/main/java/com/example/person/dto/Person.java
@@ -1,9 +1,20 @@
 package com.example.person.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Person  {
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Person {
     private int id;
     private int age;
     private int salary;
@@ -13,89 +24,5 @@ public class Person  {
     private LocalDateTime dateTimeCreate;
     private LocalDateTime timeToLunch;
     private String letter;
-
-    public Person(int id, int age, int salary, String passport, String address, LocalDate dateOfBirthday,
-                  LocalDateTime dateTimeCreate, LocalDateTime timeToLunch, String letter) {
-        this.id = id;
-        this.age = age;
-        this.salary = salary;
-        this.passport = passport;
-        this.address = address;
-        this.dateOfBirthday = dateOfBirthday;
-        this.dateTimeCreate = dateTimeCreate;
-        this.timeToLunch = timeToLunch;
-        this.letter = letter;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public String getPassport() {
-        return passport;
-    }
-
-    public void setPassport(String passport) {
-        this.passport = passport;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public LocalDate getDateOfBirthday() {
-        return dateOfBirthday;
-    }
-
-    public void setDateOfBirthday(LocalDate dateOfBirthday) {
-        this.dateOfBirthday = dateOfBirthday;
-    }
-
-    public LocalDateTime getDateTimeCreate() {
-        return dateTimeCreate;
-    }
-
-    public void setDateTimeCreate(LocalDateTime dateTimeCreate) {
-        this.dateTimeCreate = dateTimeCreate;
-    }
-
-    public LocalDateTime getTimeToLunch() {
-        return timeToLunch;
-    }
-
-    public void setTimeToLunch(LocalDateTime timeToLunch) {
-        this.timeToLunch = timeToLunch;
-    }
-
-    public String getLetter() {
-        return letter;
-    }
-
-    public void setLetter(String letter) {
-        this.letter = letter;
-    }
 }
+
